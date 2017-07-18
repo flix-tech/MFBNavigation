76f0710e1871789621bd39974835a23208f63b6b
Nikolay Kasyanov <nikolay.kasyanov@flixbus.com>
2017-07-18 17:35:15 +0200

    2 green

--- MFBNavigation/MFBUIKitUnwindToken.m
+++ MFBNavigation/MFBUIKitUnwindToken.m
@@ -7,18 +7,54 @@ typedef NS_ENUM(NSInteger, NavigationUnwindTokenState) {
     NavigationUnwindTokenStateUnwound,
 };
 
-@implementation MFBUIKitUnwindToken
+@implementation MFBUIKitUnwindToken {
+    NavigationUnwindTokenState _state;
+
+    __weak UIViewController *_unwindTarget;
+}
 
 #pragma mark - API
 
 - (void)setUnwindTarget:(UIViewController *)unwindTarget
 {
     NSCParameterAssert(unwindTarget != nil);
+
+    NSCAssert(_state != NavigationUnwindTokenStateReady, @"Unwind target has already been set");
+    NSCAssert(_state != NavigationUnwindTokenStateUnwound, @"Unwind has already happened");
+
+    if (_state == NavigationUnwindTokenStateTriggered) {
+        [_delegate unwindToTarget:unwindTarget];
+        _state = NavigationUnwindTokenStateUnwound;
+    } else {
+        _unwindTarget = unwindTarget;
+        _state = NavigationUnwindTokenStateReady;
+    }
 }
 
 - (void)unwind
 {
+    switch (_state) {
+        case NavigationUnwindTokenStateIdle:
+            _state = NavigationUnwindTokenStateTriggered;
+            return;
 
+        case NavigationUnwindTokenStateReady: {
+            __auto_type unwindTarget = _unwindTarget;
+            if (unwindTarget) {
+                [_delegate unwindToTarget:unwindTarget];
+            }
+            _state = NavigationUnwindTokenStateUnwound;
+            return;
+        }
+        case NavigationUnwindTokenStateTriggered:
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"Unwind has already been triggered"
+                                         userInfo:nil];
+        case NavigationUnwindTokenStateUnwound:
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:@"Unwind has already happened"
+                                         userInfo:nil];
+    }
 }
 
 @end
