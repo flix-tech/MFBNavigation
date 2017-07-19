26977d6dcc9699affb0932e6d99e932d45968fb6
Nikolay Kasyanov <nikolay.kasyanov@flixbus.com>
2017-07-19 14:55:33 +0200

    11

--- MFBNavigation/MFBPushPopNavigator+Test.h
+++ MFBNavigation/MFBPushPopNavigator+Test.h
@@ -3,9 +3,13 @@
 #import "MFBNavigationChildrenReplacer.h"
 #import "MFBUIKitUnwindTokenFactory.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface MFBPushPopNavigator (Test)
 
 - (void)setNavigationChildrenReplacer:(MFBNavigationChildrenReplacer *)childrenReplacer;
 - (void)setUnwindTokenFactory:(MFBUIKitUnwindTokenFactory *)unwindTokenFactory;
 
 @end
+
+NS_ASSUME_NONNULL_END
